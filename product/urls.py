8c0432f8e51935292cb690a41bd07843db5cdcef
sahar <saharkashany@gmail.com>
2022-06-13 21:27:37 +0430

    adding something

--- product/urls.py
+++ product/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.service, name='service'),
     path('template/', views.template, name='template'),
     path('sample/', views.sample, name='sample'),
-
+    # adding some thing
     #     path('addcomment/<int:id>', views.addcomment, name='addcomment'),
     #     path('colors/', views.colors, name='colors'),
 ]
